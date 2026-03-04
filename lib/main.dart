f38fedf34d1ae80eee2a72b1f5d44513d2c5651f
Naseeb Khan <110621136+Naseebullah-Khan@users.noreply.github.com>
2026-03-04 14:07:01 +0430

    Flutter Card ListTile Widgets

--- lib/main.dart
+++ lib/main.dart
@@ -13,40 +13,47 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         backgroundColor: Colors.teal,
         body: SafeArea(
-          child: Column(
-            children: [
-              CircleAvatar(
-                radius: 50.0,
-                backgroundImage: AssetImage("images/profile.jpg"),
-              ),
-              Text(
-                "Naseebullah Hoshmand",
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 40.0,
-                  fontWeight: FontWeight.bold,
-                  fontFamily: "Pacifico",
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                CircleAvatar(
+                  radius: 50.0,
+                  backgroundImage: AssetImage("images/profile.jpg"),
                 ),
-              ),
-              Text(
-                "SOFTWARE ENGINEER",
-                style: TextStyle(
-                  color: Colors.teal.shade100,
-                  fontSize: 20.0,
-                  fontFamily: "Source Sans 3",
-                  letterSpacing: 2.5,
-                  fontWeight: FontWeight.bold,
+                Text(
+                  "Naseebullah Hoshmand",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 40.0,
+                    fontWeight: FontWeight.bold,
+                    fontFamily: "Pacifico",
+                  ),
                 ),
-              ),
-              Container(
-                padding: EdgeInsets.all(10.0),
-                color: Colors.white,
-                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
-                child: Row(
-                  children: [
-                    Icon(Icons.phone, color: Colors.teal),
-                    SizedBox(width: 10.0),
-                    Text(
+                Text(
+                  "SOFTWARE ENGINEER",
+                  style: TextStyle(
+                    color: Colors.teal.shade100,
+                    fontSize: 20.0,
+                    fontFamily: "Source Sans 3",
+                    letterSpacing: 2.5,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                SizedBox(
+                  height: 20.0,
+                  width: 150.0,
+                  child: Divider(
+                    color: Colors.teal.shade100,
+                  ),
+                ),
+                Card(
+                  margin: EdgeInsets.symmetric(
+                    vertical: 10.0,
+                    horizontal: 25.0,
+                  ),
+                  child: ListTile(
+                    leading: Icon(Icons.phone, color: Colors.teal),
+                    title: Text(
                       "+93 784 449 4104",
                       style: TextStyle(
                         color: Colors.teal.shade900,
@@ -54,49 +61,44 @@ class MyApp extends StatelessWidget {
                         fontSize: 20.0,
                       ),
                     ),
-                  ],
+                  ),
                 ),
-              ),
-              Container(
-                padding: EdgeInsets.all(10.0),
-                color: Colors.white,
-                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
-                child: Row(
-                  children: [
-                    Icon(Icons.email, color: Colors.teal),
-                    SizedBox(width: 10.0),
-                    Text(
+                Card(
+                  margin: EdgeInsets.symmetric(
+                    vertical: 10.0,
+                    horizontal: 25.0,
+                  ),
+                  child: ListTile(
+                    leading: Icon(Icons.email, color: Colors.teal),
+                    title: Text(
                       "naseebullah_hoshmand@hotmail.com",
                       style: TextStyle(
                         color: Colors.teal.shade900,
                         fontFamily: "Source Sans 3",
-                        fontSize: 15.0,
+                        fontSize: 16.0,
                       ),
                     ),
-                  ],
+                  ),
                 ),
-              ),
-              Container(
-                padding: EdgeInsets.all(10.0),
-                color: Colors.white,
-                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
-                child: Row(
-                  children: [
-                    Icon(Icons.public, color: Colors.teal),
-                    SizedBox(width: 10.0),
-                    Text(
+                Card(
+                  margin: EdgeInsets.symmetric(
+                    vertical: 10.0,
+                    horizontal: 25.0,
+                  ),
+                  child: ListTile(
+                    leading: Icon(Icons.public, color: Colors.teal),
+                    title: Text(
                       "https://naseebullah-hoshmand.vercel.app/",
                       style: TextStyle(
                         color: Colors.teal.shade900,
                         fontFamily: "Source Sans 3",
-                        fontSize: 15.0,
+                        fontSize: 14.0,
                       ),
                     ),
-                  ],
+                  ),
                 ),
-              ),
-            ],
-          ),
+              ],
+            ),
         ),
       ),
     );
