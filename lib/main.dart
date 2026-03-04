5db87423f53cadd77b1e3be51287b58bc76007fe
Naseeb Khan <110621136+Naseebullah-Khan@users.noreply.github.com>
2026-03-04 13:34:04 +0430

    Adding Material Icons with the Icon Widget

--- lib/main.dart
+++ lib/main.dart
@@ -38,6 +38,63 @@ class MyApp extends StatelessWidget {
                   fontWeight: FontWeight.bold,
                 ),
               ),
+              Container(
+                padding: EdgeInsets.all(10.0),
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                child: Row(
+                  children: [
+                    Icon(Icons.phone, color: Colors.teal),
+                    SizedBox(width: 10.0),
+                    Text(
+                      "+93 784 449 4104",
+                      style: TextStyle(
+                        color: Colors.teal.shade900,
+                        fontFamily: "Source Sans 3",
+                        fontSize: 20.0,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Container(
+                padding: EdgeInsets.all(10.0),
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                child: Row(
+                  children: [
+                    Icon(Icons.email, color: Colors.teal),
+                    SizedBox(width: 10.0),
+                    Text(
+                      "naseebullah_hoshmand@hotmail.com",
+                      style: TextStyle(
+                        color: Colors.teal.shade900,
+                        fontFamily: "Source Sans 3",
+                        fontSize: 15.0,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Container(
+                padding: EdgeInsets.all(10.0),
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                child: Row(
+                  children: [
+                    Icon(Icons.public, color: Colors.teal),
+                    SizedBox(width: 10.0),
+                    Text(
+                      "https://naseebullah-hoshmand.vercel.app/",
+                      style: TextStyle(
+                        color: Colors.teal.shade900,
+                        fontFamily: "Source Sans 3",
+                        fontSize: 15.0,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
             ],
           ),
         ),
